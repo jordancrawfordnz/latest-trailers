7b39b26d4bef99c0f3c5eb931d7b56d07e583a82
Jordan Crawford <jordan.crawford2@powershop.co.nz>
2017-03-07 21:20:59 +1300

    Setup the fetcher to upload to S3.

--- fetcher/index.js
+++ fetcher/index.js
@@ -1,9 +1,30 @@
 var Promise = require('promise');
-const MovieDB = require('moviedb')('8f9dc61dae4de5e1d69cf48ab6be2c64');
+var s3 = require('s3');
+var tmp = require('tmp');
+var jsonfile = require('jsonfile')
 
 const FETCH_VIDEOS_DELAY = 400;
 
+if (process.argv.length !== 3) {
+  console.log('node index.js [config path]');
+  return;
+}
+
+var envFilePath = process.argv[2];
+var env = jsonfile.readFileSync(envFilePath);
+
+var s3Client = s3.createClient({
+  s3Options: {
+    accessKeyId: env.accessKeyId,
+    secretAccessKey: env.secretAccessKey,
+    region: env.region
+  }
+});
+
+const MovieDB = require('moviedb')(env.tmdbKey);
+
 var upcomingMovies = Promise.denodeify(MovieDB.miscUpcomingMovies.bind(MovieDB));
+var nowPlayingMovies = Promise.denodeify(MovieDB.miscNowPlayingMovies.bind(MovieDB));
 var movieVideos = Promise.denodeify(MovieDB.movieVideos.bind(MovieDB));
 
 function getTrailer(movie) {
@@ -11,9 +32,22 @@ function getTrailer(movie) {
   return movieVideos({ id: movie.id }).then(function(response) {
     process.stdout.write('Done.\n');
 
-    // TODO: Filter to Trailers and respond with the movie ID and a list of trailers.
+    var trailers = response.results.filter(function(video) {
+      return video.type === "Trailer";
+    });
 
-    return response.results;
+    if (trailers.length > 0) {
+      var trailerKeys = trailers.map(function(trailer) {
+        return trailer.key;
+      });
+
+      return {
+        movieId: movie.id,
+        trailerKeys: trailerKeys
+      }
+    } else {
+      return null;
+    }
   });
 }
 
@@ -25,7 +59,9 @@ function getTrailers(remainingMovies, movieTrailers) {
   if (remainingMovies.length > 0) {
     var currentMovie = remainingMovies.shift();
     return getTrailer(currentMovie).then(function(trailers) {
-      movieTrailers.push(trailers);
+      if (trailers) {
+        movieTrailers.push(trailers);
+      }
 
       return new Promise(function(resolve, reject) {
         setTimeout(function() {
@@ -44,12 +80,49 @@ function loadMovieList(queryFunction) {
   });
 }
 
-loadMovieList(upcomingMovies).then(function(trailers) {
-  console.log('Done :)');
-  console.log(trailers);
+function uploadToS3(file, filePath) {
+  return new Promise(function(resolve, reject) {
+    var uploadParams = {
+      localFile: file.name,
+      s3Params: {
+        Bucket: 'latesttrailerstest',
+        Key: filePath
+      }
+    };
 
-  // TODO: Build a file and upload it to Amazon S3.
+    var uploader = s3Client.uploadFile(uploadParams);
+    uploader.on('error', function(err) {
+      reject(err);
+    });
+
+    uploader.on('end', function() {
+      resolve();
+    });
+  });
+}
+
+function getTempJSON(content) {
+  var tempFile = tmp.fileSync();
+  jsonfile.writeFileSync(tempFile.name, content);
+
+  return tempFile;
+}
+
+function uploadTrailerDetails(queryFunction, filename) {
+  return loadMovieList(queryFunction).then(function(trailers) {
+    var file = getTempJSON(trailers);
+    return uploadToS3(file, filename).then(function() {
+      console.log('Uploaded ' + filename + ' successfully!');
+    });
+  });
+}
+
+
+uploadTrailerDetails(upcomingMovies, 'upcoming.json').then(function() {
+  return uploadTrailerDetails(nowPlayingMovies, 'now-playing.json').then(function() {
+    console.log('Fetch completed.');
+  });
 }, function(error) {
   console.log('An error occurred.');
   console.log(error);
-});
+})
